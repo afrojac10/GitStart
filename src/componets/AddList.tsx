1e5de5a2770996b0b041091add89752295cfdf06
Blaise Njike <40248358+afrojac10@users.noreply.github.com>
2024-05-17 13:34:26 +0100

    Update AddList.tsx

--- src/componets/AddList.tsx
+++ src/componets/AddList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
-import Added from "./Added";
+// import Added from "./Added";
 
 export default function AddList() {
   /**
