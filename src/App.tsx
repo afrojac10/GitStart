35517ecd664eff78c45080b9a4d3e5b2acfae82c
Blaise Njike <40248358+afrojac10@users.noreply.github.com>
2024-05-17 13:31:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./componets/Header";
 import KanBan from "./componets/KanBanBord";
 import ListTask from "./componets/ListTask";
 import AddList from "./componets/AddList";
-import InputTask from "./componets/InputTask";
+//import InputTask from "./componets/InputTask";
 
 function App() {
   return (
